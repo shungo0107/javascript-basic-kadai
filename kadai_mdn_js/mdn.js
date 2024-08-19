7c8a4a69706120449363b343b81d607fd74f22db
shungo0107 <odaira.sh@icloud.com>
2024-08-19 21:12:12 +0900

    課題提出のため

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+
+const selectDate = () =>{
+    const today = new Date();
+    console.log(today.getFullYear() + '年' 
+                 + today.getMonth() + '月' 
+                 + today.getDate() + '日');
+}
+
+selectDate();
